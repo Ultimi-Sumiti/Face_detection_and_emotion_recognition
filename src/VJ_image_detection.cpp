d7d268c7e17ddcdd4830c3771e2dca2c9a716c67
Ale10chine <alex10.chinello@gmail.com>
2025-09-09 10:54:45 +0200

    Added the folder for the sharing cropped_imgs and first implementation of detection

--- src/VJ_image_detection.cpp
+++ src/VJ_image_detection.cpp
@@ -11,13 +11,13 @@ int main(void){
     cv::CascadeClassifier face_cascade;
 
     //-- 1. Load the cascades
-    if (!face_cascade.load("../classifiers/haarcascade_frontalface_alt.xml"))
-    {
+    if (!face_cascade.load("../classifiers/haarcascade_frontalface_alt.xml")){
+
         std::cout << "--(!)Error loading face cascade\n";
         return -1;
     };
 
-    std::string img_path = "../dataset_detection/images/disgust (2).jpg";
+    std::string img_path = "../dataset_detection/images/angry (1).jpg";
     cv::Mat img = cv::imread(img_path);
     detectAndDisplay(img, face_cascade);
 
@@ -26,29 +26,49 @@ int main(void){
 }
 
 
-void detectAndDisplay( cv::Mat frame , cv::CascadeClassifier f_cascade)
-{
+void detectAndDisplay( cv::Mat frame , cv::CascadeClassifier f_cascade){
+    
     cv::Mat frame_gray;
+    // Convert into GRAY the frame passed.
     cv::cvtColor( frame, frame_gray, cv::COLOR_BGR2GRAY );
-    cv::equalizeHist( frame_gray, frame_gray ); // Histogram equalization
+    // Histogram equalization.
+    cv::equalizeHist( frame_gray, frame_gray ); 
+
+    // Plotting the image equalized.
     cv::namedWindow("Window");
     cv::imshow( "Window", frame_gray );
     cv::waitKey(0);
 
-    //-- Detect faces
+    //-- Detect faces on the frame in gray scale.
     std::vector<cv::Rect> faces;
     f_cascade.detectMultiScale( frame_gray, faces );
 
+    // Folder path in which will be saved the images
+    std::string folder_path_cropped_imgs = "../cropped_imgs/";
+    // Vector of cropped images
+    std::vector<cv::Mat>  cropped_imgs; 
+    for (size_t i = 0; i < faces.size(); i++){
 
-    for ( size_t i = 0; i < faces.size(); i++ )
-    {
         cv::Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
         cv::Rect face_rect(faces[i].x, faces[i].y, faces[i].width, faces[i].height);
-        cv::rectangle(frame, face_rect, cv::Scalar(255, 0, 255), 4);  // colore e spessore
+        // Draw the box over the detection
+        cv::rectangle(frame, face_rect, cv::Scalar(255, 0, 255), 4);  
 
         cv::Mat faceROI = frame_gray( faces[i] );
+        cropped_imgs.push_back(faceROI.clone());
+
+        cv::imwrite(folder_path_cropped_imgs + "cut_" + std::to_string(i)+".png", cropped_imgs[i]);        
     }
+
     //-- Show what you got
     cv::imshow( "Window", frame );
+    cv::waitKey(0);
+
+    for (size_t i = 0; i < cropped_imgs.size(); i++){
+
+        std::string window_name = "Window " + std::to_string(i);
+        cv::imshow(window_name, cropped_imgs[i]);
+    }
+
     cv::waitKey(0);
 }
