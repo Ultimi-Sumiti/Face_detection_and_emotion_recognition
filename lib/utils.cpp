87e5f71671adde9bd1509548a0360e8b512781ac
Daedalus02 <mattia.scantamburlo02@gmail.com>
2025-09-10 18:48:53 +0200

    d

--- lib/utils.cpp
+++ lib/utils.cpp
@@ -164,11 +164,14 @@ std::vector<cv::Rect> face_detect(cv::Mat& frame){
     std::vector<cv::Rect> filtered_faces; 
     std::vector<cv::Rect> best_detections; 
     int min_score = 1000000;
+    int min_area = frame.rows * frame.cols /150;
     int best_score = 0;
     int best_index = 0;
     int actual_score = 0;
     int best_count = 0;
-    int min_area = frame.rows * frame.cols /100 ;
+    float blur_score = 0.0f;
+    cv::Rect img_rect = cv::Rect(0,0, frame.cols,frame.rows);
+    float avg_blur = calculateBlurScore(frame, img_rect);
 
     // Checking all possible faces cascades. 
     for(int i = 0; i < f_cascades.size(); i++){
@@ -192,6 +195,7 @@ std::vector<cv::Rect> face_detect(cv::Mat& frame){
         // Loop through each detected face.
         for(int j = 0; j < faces.size(); j++){
             // Print the confidence score for the corresponding face.
+
             score = calculateBlurScore(frame, faces[j]) * faces[j].area();
             std::cout << "Face " << j
                 << " -> Score: " << score <<std::endl;
