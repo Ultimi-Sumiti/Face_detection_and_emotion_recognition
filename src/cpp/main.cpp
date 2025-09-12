0bf23bad857f6486aa204f93d335868ea60dca22
luca <lucapiai98@proton.me>
2025-09-12 16:35:54 +0200

    Minor changes

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -10,13 +10,8 @@
 #include "../../include/face_detector.h"
 
 
-// Function used to run the emotion recognition model (in Python).
-void run_emotion_rec() {
-    int ret = system("python3 ../src/python/emotion_classifier.py 2>../recognition_output.txt");
-}
-
-
-const std::vector<std::string> classifiers_paths = {
+// Haarcascades that can be used for face detection.
+const std::vector<std::string> HAARCASCADES_PATHS = {
     "../data/haarcascades/haarcascade_frontalface_alt.xml",
     "../data/haarcascades/haarcascade_frontalface_alt_tree.xml",
     "../data/haarcascades/haarcascade_frontalface_default.xml",
@@ -24,30 +19,50 @@ const std::vector<std::string> classifiers_paths = {
     "../data/haarcascades/haarcascade_profileface.xml",
 };
 
-// Folder path in which will be saved the images.
-const std::string folder_path_cropped_imgs = "../tmp/cropped_imgs/";
-const std::string detections_path = "../output/detections/";
-const std::string image_extension = ".jpg";
+// Folder path in which cropped images with faces are (temporary) stored.
+const std::string CROPPED_IMGS_PATH = "../tmp/cropped_imgs/";
 
+// Folder in which the images with the bounding boxes drawn are stored.
+const std::string OUTPUT_DETECTIONS_PATH = "../output/detections/";
+// File where metrics are stored.
+const std::string METRICS_OUT = "../output/metrics.txt";
+
+// Path to the fifo file where messages are sent.
 const std::string SEND_FIFO = "../tmp/cpp_to_py.fifo";
+// Path to the fifo file where messages are received.
 const std::string RECEIVE_FIFO = "../tmp/py_to_cpp.fifo";
+
+// Command used to start the emotion recognition model.
+const std::string EMOTION_REC_CMD = "python3 ../src/python/emotion_classifier.py"
+                                    " 2> ../log/emotion_classifier_log.txt";
+
+// Function used to run the emotion recognition model (in Python).
+void run_emotion_rec() {
+    int ret = system(EMOTION_REC_CMD.c_str());
+}
 
 
 int main(int argc, char* argv[]) {
 
-    // Clean all images in the output dir from previous run.
-    remove_images(get_all_filenames(detections_path));
+    // Create fifo files used for Inter Process Communication (CPP, Python).
+    if (fifo_creation(SEND_FIFO) || fifo_creation(RECEIVE_FIFO)) {
+        std::cerr << "Error in creating the fifo file... aborting" << std::endl;
+        return EXIT_FAILURE;
+    }
 
-    // Create fifo files for Inter Process Communication (CPP, Python).
-    fifo_creation(SEND_FIFO);
-    fifo_creation(RECEIVE_FIFO);
+    // Clean all images in the output dir from previous run.
+    remove_images(get_all_filenames(OUTPUT_DETECTIONS_PATH));
 
     // Parse command line, get image directory and (optinally) labels directory.
     std::string imgs_dir_path{}, labels_dir_path{};
-    parse_command_line(argc, argv, imgs_dir_path, labels_dir_path);
+    if (parse_command_line(argc, argv, imgs_dir_path, labels_dir_path)) {
+        std::cout << help_msg << std::endl;
+        return EXIT_FAILURE;
+    }
 
     if (imgs_dir_path.empty()) {
-        std::cerr << "Error in parsing the command line...\n";
+        std::cerr << "Error: input directory argument is missing." << std::endl;
+        std::cout << help_msg << std::endl;
         return EXIT_FAILURE;
     }
 
@@ -57,15 +72,22 @@ int main(int argc, char* argv[]) {
     if (!labels_dir_path.empty())
         labels_paths = get_all_filenames(labels_dir_path);
 
+    if (imgs_paths.empty()) {
+        std::cerr << "ERROR: directory '" << imgs_dir_path 
+                  << "' is empty or doesn't exists." << std::endl;
+        return EXIT_FAILURE;
+    }
 
-    // -------------------------------------- FACE DETECTION --------------------------------------
+
+    // ------------------------- FACE DETECTION -------------------------------
 
     // Define the FaceDetector passing it the path of the classifier to load.
     FaceDetector detector;
     try {
-        detector = FaceDetector(classifiers_paths);
+        detector = FaceDetector(HAARCASCADES_PATHS);
     } catch(const std::runtime_error& e) {
-        std::cerr << "Exception caught, impossible to upload the cascades: " << e.what() << std::endl;
+        std::cerr << "Exception caught, impossible to upload the cascades: " 
+                  << e.what() << std::endl;
         return EXIT_FAILURE;
     }
 
@@ -90,15 +112,15 @@ int main(int argc, char* argv[]) {
         std::vector<cv::Rect> faces = detector.face_detect(img);
         std::cout<<std::endl<<"Detected: "<< faces.size()<< " faces."<<std::endl;
         // Crop images and save it in a vector.
-        std::vector<std::string> cropped_paths = crop_images(img, faces, folder_path_cropped_imgs);
+        std::vector<std::string> cropped_paths = crop_images(img, faces, CROPPED_IMGS_PATH);
 
-        // ------------------------------------ EMOTION RECOGNITION ------------------------------------
+        // -------------------- EMOTION RECOGNITION ---------------------------
         // Signal (to Python).
         std::cout<<"Prima di python\n";   
         std::ofstream to_server(SEND_FIFO);
 
         if(faces.empty()){
-            std::cout <<"No faces are detected, going to next image\n";
+            std::cout <<"No faces are detected, going to next image." << std::endl;
             // Singal (to Python) for closing its pipeline. 
             to_server << "continue" << std::endl; 
             // Go to next iteration (next image).
@@ -126,36 +148,24 @@ int main(int argc, char* argv[]) {
         detector.draw_bbox(img, faces, labels);
 
         // Store the image with boxes drawn.
-        std::string full_detection_path = detections_path + "image_" + std::to_string(itr) + image_extension;
+        std::string full_detection_path = OUTPUT_DETECTIONS_PATH + "image_" + std::to_string(itr) + ".png";
         if (cv::imwrite(full_detection_path, img))
             std::cout << "Image: " << full_detection_path << " saved." << std::endl;
         else
             std::cerr << "Error: couldn't save " << full_detection_path << " to disk" << std::endl;
 
-        //  ------------------------------------ PERFORMANCE METRICS ------------------------------------ 
-        // Performance metrics, if necessary.
-        //if (!file_name.empty()) {
-        //    std::vector<cv::Rect> label_rect = compute_rectangles(labels_paths.back(), img.cols, img.rows);
-        //    PerformanceMetrics pm(faces, label_rect);
-        //    pm.print_metrics();
-        //}
-
-        //draw_bbox(img, faces, labels);
-        //namedWindow("Window", cv::WINDOW_NORMAL);
-        //cv::imshow("Window", img);
-        //cv::waitKey(0);
+        //  ------------------ PERFORMANCE METRICS ----------------------------
 
         // Compute and store metrics in a file, if necessary.
         if (!labels_paths.empty()) {
             labels_rect = compute_rectangles(labels_paths[itr], img.cols, img.rows);
-            PerformanceMetrics pm = PerformanceMetrics(faces, labels_rect);
+            PerformanceMetrics pm = PerformanceMetrics(faces, labels_rect, METRICS_OUT);
             if (itr == 0) pm.clean_metrics();
             pm.print_metrics(imgs_paths[itr]);
         }
 
         // Remove cropped images.
         remove_images(cropped_paths); 
-
     }
 
     // Sending exit message to python.
